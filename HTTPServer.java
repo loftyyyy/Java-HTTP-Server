5aaa92f9d93b6c1ba9c9a08d488b38d61896b496
loftyyyy <lofty08.business@gmail.com>
2024-04-03 20:05:22 +0800

    First commit

---
+++ HTTPServer.java
@@ -0,0 +1,12 @@
+public class HTTPServer {
+
+    public static void main(String[] args){
+
+        HTTPServer main = new HTTPServer();
+        main.Server();
+    }
+
+    public void Server(){
+
+    }
+}
